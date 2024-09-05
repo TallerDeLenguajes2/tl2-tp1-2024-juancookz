3f647423e376d9c8b9e9030af9f2c0023a321c7b
juancookz <jcavila383@gmail.com>
2024-09-05 12:47:03 -0300

    Se genera constructor

--- Cadete.cs
+++ Cadete.cs
@@ -1,11 +1,20 @@
 class Cadete
 {
-    private string id;
+    private int Id;
     private string Nombre;
     private string Direccion;
     private string Telefono;
+
+    public Cadete(int id, string nombre, string direccion, string telefono)
+    {
+        Id = id;
+        Nombre = nombre;
+        Direccion = direccion;
+        Telefono = telefono;
+    }
+
     public void Mostrar()
     {
-        System.Console.WriteLine(id + " | " + Nombre);
+        System.Console.WriteLine(Id + " | " + Nombre);
     }
 }
