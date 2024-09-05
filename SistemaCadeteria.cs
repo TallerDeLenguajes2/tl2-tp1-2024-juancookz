355348eb86a9be7e448f806972acf664a5268851
juancookz <jcavila383@gmail.com>
2024-09-05 12:48:53 -0300

    Se implementa funcion para leeir lista de cadetes desde un CSV

--- SistemaCadeteria.cs
+++ SistemaCadeteria.cs
@@ -1,6 +1,9 @@
-Cadeteria cadeteria = new Cadeteria("Don pepe", "3813524023");
 IdGen idCadetes = new IdGen("idCadetes.csv");
 IdGen idPedidos = new IdGen("idPedidos.csv");
+List<Cadete> cadetes = LeerListaCadetesCsv("cadetes.csv");
+List<Pedido> pedidos = new List<Pedido>();
+Cadeteria cadeteria = new Cadeteria("Pepe","3813524023",cadetes, pedidos);
+
 
 bool continuar = true;
 Console.Clear();
@@ -51,4 +54,23 @@ static int elegirOpcion()
         entrada = Console.ReadLine();
     }
     return salida;
+}
+
+List<Cadete> LeerListaCadetesCsv(string cadetesFilePath)
+{
+    CSV csvHandler = new CSV();
+    List<string> ListaStringCadetes = csvHandler.ReadCSV(cadetesFilePath);
+    List<Cadete> ListadoCadetesCsv = new List<Cadete>();
+    foreach (var cadeteData in ListaStringCadetes)
+    {
+        string[] dataFields = cadeteData.Split(',');
+        int id = int.Parse(dataFields[0]);
+        string nombre = dataFields[1];
+        string direccion = dataFields[2];
+        string telefono = dataFields[3];
+
+        Cadete cadete = new Cadete(id, nombre, direccion, telefono);
+        ListadoCadetesCsv.Add(cadete);
+    }
+    return ListadoCadetesCsv;
 }
