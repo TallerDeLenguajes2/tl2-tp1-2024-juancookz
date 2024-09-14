3f3769a4cc3982c040bbf3c91edbaa40fd836a8c
juancookz <jcavila383@gmail.com>
2024-09-13 21:20:35 -0300

    Funcion CantEntregasCadete ahora es private

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -49,7 +49,7 @@ class Cadeteria
             return 0;
         }
     }
-    public int CantEntregasCadete(int idCadete)
+    private int CantEntregasCadete(int idCadete)
     {
         return ListadoPedidos
             .Where(p => p.Cadete.Id == idCadete && p.estado == Pedido.Estado.Finalizado)
